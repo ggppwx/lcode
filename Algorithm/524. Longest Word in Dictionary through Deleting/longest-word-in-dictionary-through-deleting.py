74e67287ce79826eea95677d7044e93961dc6f57
ggppwx <sirgujingwei@gmail.com>
2019-03-03 16:17:02 -0500

    Update longest-word-in-dictionary-through-deleting.py

--- Algorithm/524. Longest Word in Dictionary through Deleting/longest-word-in-dictionary-through-deleting.py
+++ Algorithm/524. Longest Word in Dictionary through Deleting/longest-word-in-dictionary-through-deleting.py
@@ -2,7 +2,9 @@
 Problem
 Longest Word in Dictionary through Deleting
 (https://leetcode.com/problems/longest-word-in-dictionary-through-deleting)
-Given a string and a string dictionary, find the longest string in the dictionary that can be formed by deleting some characters of the given string. If there are more than one possible results, return the longest word with the smallest lexicographical order. If there is no possible result, return the empty string.
+Given a string and a string dictionary, find the longest string in the dictionary that can be formed by deleting some characters of the
+given string. If there are more than one possible results, return the longest word with the smallest lexicographical order. If there is
+no possible result, return the empty string.
 
 Example 1:
 Input:
