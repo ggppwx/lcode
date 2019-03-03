c60212b21ec115144f18e6dfdce151004e358602
ggppwx <sirgujingwei@gmail.com>
2019-03-03 15:20:22 -0500

    Update koko-eating-bananas.py

--- Algorithm/875. Koko Eating Bananas/koko-eating-bananas.py
+++ Algorithm/875. Koko Eating Bananas/koko-eating-bananas.py
@@ -3,16 +3,13 @@ Problem
 Koko Eating Bananas
 (https://leetcode.com/problems/koko-eating-bananas)
 
-Koko loves to eat bananas.  There are N piles of bananas, the i-th pile has piles[i] bananas.  The guards have gone and will come back in H hours.
-
-Koko can decide her bananas-per-hour eating speed of K.  Each hour, she chooses some pile of bananas, and eats K bananas from that pile.  If the pile has less than K bananas, she eats all of them instead, and won't eat any more bananas during this hour.
-
+Koko loves to eat bananas.  There are N piles of bananas, the i-th pile has piles[i] bananas.  
+The guards have gone and will come back in H hours.
+Koko can decide her bananas-per-hour eating speed of K.  Each hour, she chooses some pile of bananas, 
+and eats K bananas from that pile.  If the pile has less than K bananas, she eats all of them instead, 
+and won't eat any more bananas during this hour.
 Koko likes to eat slowly, but still wants to finish eating all the bananas before the guards come back.
-
 Return the minimum integer K such that she can eat all the bananas within H hours.
-
- 
-
 Example 1:
 
 Input: piles = [3,6,7,11], H = 8
@@ -31,7 +28,17 @@ Output: 23
 import unittest
 import math
 class Solution:
-    # copy the funtion here
+    """
+    Binary serach to find the two pile which the target number sits in between
+    then do another bianry search to get the actual target number.
+    A easier approach is:
+    start = 1
+    end = max(piles).
+    directly do bianry serach
+    
+    special case: start == end, target = start
+    if meet the condition, the target is in [start, mid], otherwise (mid, end]
+    """
     def minEatingSpeed(self, piles: 'List[int]', H: 'int') -> 'int':
         piles = sorted(piles)
         low = 0
