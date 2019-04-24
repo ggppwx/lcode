9d6ab08e66e0c21e70e7a074bf5d5d11c6066817
roy gu <sirgujingwei@gmail.com>
2019-04-24 17:21:46 -0400

    update lcode

--- publish.sh
+++ publish.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-git add README.md
-git add Algorithm
-
 bash ./timestamp.sh ;
 
 # refresh readme
 python3 get.py -r
 
 # publish the change to github
-
+git add README.md
+git add Algorithm
 git commit -m 'update lcode'
 git push 
 
