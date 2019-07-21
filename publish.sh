795a3b179219634b2af14d6e8b4409b9054d418e
roy gu <sirgujingwei@gmail.com>
2019-07-21 17:42:46 -0400

    update lcode

--- publish.sh
+++ publish.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
+git config --global core.autocrlf true
 bash ./timestamp.sh ;
 
 # refresh readme
 python3 get.py -r
 
 # publish the change to github
-git config --global core.autocrlf true
 git add README.md
 git add Algorithm
 git commit -m 'update lcode'
