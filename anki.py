bc358a3cd7c8c6c787424c04220a589a1db4d7d3
roy gu <sirgujingwei@gmail.com>
2019-09-20 18:10:29 -0400

    comit

--- anki.py
+++ anki.py
@@ -1,24 +1,55 @@
 """
 the anki converter
+first column: the name of the question
+second column: the solution (analysis)
+third column
 """
 import argparse
 
 def md_to_anki_csv(file):
-    pass
+    is_question = False
+    is_ans = False
+    question = ""
+    ans = ""
+    csv_line = ""
+    with open(file) as in_file, open('out.csv', 'a') as out_file:
+        for line in in_file:
+            if line.startswith('# '):
+                is_question = True
+                continue
+
+            if line.startswith('##'):
+                is_question = False
+                is_ans = False
+
+                if '## Analysis' in line:
+                    is_ans = True
+                    continue
+
+            if is_question:
+                question += line
+                is_question = False
+
+            if is_ans:
+                ans += line
+
+        csv_line = "\"{}\" , \"{}\"\n".format(question, ans)
+        out_file.write(csv_line)
+
 
 
 
 def main():
-    parser = argparse.ArgumentParser()    
+    parser = argparse.ArgumentParser()
     parser.add_argument('-m', '--mode', help='converter mode')
-    parser.add_argument('file', type=str)
+    parser.add_argument('file', nargs='+', type=str)
     args = parser.parse_args()
 
-    
+
     if args.mode == 'csv':
         # convert the md to csv falsh card
-        file_path = args.file
-        md_to_anki_csv(file_path)
+        for file_path in args.file:
+            md_to_anki_csv(file_path)
 
 
 if __name__ == '__main__':
